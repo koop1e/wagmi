0e7da58a04c6beb4d7024e0c586e1fe01589817d
dayu341 <dayu.gai@gmail.com>
2023-07-09 16:02:56 +0930

    Making changes

--- src/App.js
+++ src/App.js
@@ -1,80 +1,106 @@
 import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
-import { useAccount, useConnect, useDisconnect, useBalance } from 'wagmi'
+
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
-import 'bootstrap/dist/css/bootstrap.min.css'; // import Bootstrap CSS
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
-const { chains, publicClient } = configureChains(
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from 'wagmi'
+ 
+export function Profile() {
+  const { address, connector, isConnected } = useAccount()
+  const { data: ensAvatar } = useEnsAvatar({ address })
+  const { data: ensName } = useEnsName({ address })
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect()
+  const { disconnect } = useDisconnect()
+ 
+  if (isConnected) {
+    return (
+      <div>
+        <img src={ensAvatar} alt="ENS Avatar" />
+        <div>{ensName ? `${ensName} (${address})` : address}</div>
+        <div>Connected to {connector.name}</div>
+        <button onClick={disconnect}>Disconnect</button>
+      </div>
+    )
+  }
+ 
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+        >
+          {connector.name}
+          {!connector.ready && ' (unsupported)'}
+          {isLoading &&
+            connector.id === pendingConnector?.id &&
+            ' (connecting)'}
+        </button>
+      ))}
+ 
+      {error && <div>{error.message}</div>}
+    </div>
+  )
+}
+
+
+
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet],
-  [
-    jsonRpcProvider({
-      rpc: () => ({
-        http: "https://alpha-rpc.scroll.io/l2	" // 👈 Replace this with your HTTP URL from the previous step
-      }),
-    })
-  ]
-);
+  [publicProvider()],
+)
 
+// Set up wagmi config
 const config = createConfig({
   autoConnect: true,
-  publicClient,
   connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: '...',
+      },
+    }),
     new InjectedConnector({
       chains,
       options: {
         name: 'Injected',
         shimDisconnect: true,
       },
-    })
-  ]
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
 })
 
-
-function Profile() {
-  const { address } = useAccount()
-  const { connect, isConnecting } = useConnect({
-    connector: new InjectedConnector(),
-  })
-  const { disconnect } = useDisconnect()
-  const { data, isError, isLoading } = useBalance({
-    address: address,
-  })
-
-  if (address) {
-    return (
-      <div className="d-flex justify-content-center align-items-center vh-100">
-        <div className="text-center">
-          <p>Connected to {address}</p>
-          <p>Balance: {data ? data.formatted : "Loading..."} ETH</p>
-          <p>Chain ID: {config ? config.lastUsedChainId : ""}</p>
-          <button className="btn btn-primary mt-3" onClick={disconnect}>Disconnect</button>
-        </div>
-      </div>
-    )
-  }
-
-  if (isConnecting) {
-    return (
-      <div className="d-flex justify-content-center align-items-center vh-100">
-        <p>Connecting...</p>
-      </div>
-    )
-  }
-
-
-  return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <button className="btn btn-primary" onClick={() => connect()}>Connect Wallet</button>
-    </div>
-  )
-}
-
+// Pass config to React Context Provider
 function App() {
   return (
     <WagmiConfig config={config}>
-      <Profile/>
+      <Profile />
     </WagmiConfig>
   )
 }
 
-export default App;
+export default App
